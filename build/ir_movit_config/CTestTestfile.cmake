d6afc371f327a6536a49dcc46972ec2e101665c1
lucordes <lucacordes@lucacordes.com>
2025-11-24 16:18:48 +0100

    remove cached

--- build/ir_movit_config/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/michele-zarantonello/ws_32_assignments/src/ir_2526/ir_movit_config
-# Build directory: /home/michele-zarantonello/ws_32_assignments/build/ir_movit_config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/ir_movit_config/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/michele-zarantonello/ws_32_assignments/install/ir_desription/share/ir_desription/package.sh"
