59c9ef70c3a059dc3c0608e4b508cf9e7d3d7a9a
groricc <rikygr@libero.it>
2025-11-28 18:57:39 +0100

    Fixed some problems of the table detector

--- src/group_32_assignment_1/src/table_detection.cpp
+++ src/group_32_assignment_1/src/table_detection.cpp
@@ -45,28 +45,30 @@ private:
          }
          else if (current_distance - previous_distance > distance_threshold && possible_minimum)
          {
-            // Confirmed end of a table
-            table_count++;
-
-            // Computing central point of the detected table
+            
+            // Computing dimensions of the detected table
             end_table_index = i - 1;
             double angle_cluster = (end_table_index - start_table_index) * laser_msg->angle_increment; // Angle of the arc detecting table
-            int central_index = (start_table_index + end_table_index) / 2;
-            double central_distance = laser_msg->ranges[central_index];
-            double central_angle = laser_msg->angle_min + central_index * laser_msg->angle_increment;
 
-            // Estimation of table radius
-            double table_radius = 1.5 * angle_cluster/M_PI * std::pow(central_distance,2.0);
-            RCLCPP_INFO(this->get_logger(), "Table %d: Central Angle: %.2f deg, Central Distance: %.2f m, Estimated Radius: %.2f m",
+            // Check if the arc length is eligible to be considered a table (more robust to specific cases)
+            if(angle_cluster < M_PI/3.0)
+            {
+               table_count++;
+               // Calculation of table radius
+               int central_index = (start_table_index + end_table_index) / 2;
+               double central_distance = laser_msg->ranges[central_index];
+               double central_angle = laser_msg->angle_min + central_index * laser_msg->angle_increment;
+               double table_radius = (previous_distance * tan(angle_cluster / 2.0));
+               RCLCPP_INFO(this->get_logger(), "Table %d: Central Angle: %.2f deg, Central Distance: %.2f m, Estimated Radius: %.2f m",
                         table_count, central_angle*180/M_PI, central_distance, table_radius);
             
-            // Calculating coordinates of the table center w.r.t. the scanner reference frame
-            double x_table = (central_distance + table_radius) * cos(central_angle);
-            double y_table = (central_distance + table_radius) * sin(central_angle);
-            RCLCPP_INFO(this->get_logger(), "Center Coordinates: (%.2f, %.2f) m", x_table, y_table);
+               // Calculating coordinates of the table center w.r.t. the scanner reference frame
+               double x_table = (central_distance + table_radius) * cos(central_angle);
+               double y_table = (central_distance + table_radius) * sin(central_angle);
+               RCLCPP_INFO(this->get_logger(), "Center Coordinates: (%.2f, %.2f) m", x_table, y_table);
+            }
             possible_minimum = 0; // Reset for next detection
          }
-         
       }
       RCLCPP_INFO(this->get_logger(), "Detected %d tables.", table_count);
    }
